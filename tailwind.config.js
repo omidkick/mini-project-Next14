4410f3481b74ded2d0bb71f4a9a8f6c4c92b79e5
Omid Jabbari <omidjabbri0041@gmail.com>
2025-11-24 09:34:46 +0330

    add some dark and light mode color in globals.css and start feature based for my project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          50: "rgb(var(--color-primary-50))",
+          100: "rgb(var(--color-primary-100))",
+          200: "rgb(var(--color-primary-200))",
+          300: "rgb(var(--color-primary-300))",
+          400: "rgb(var(--color-primary-400))",
+          500: "rgb(var(--color-primary-500))",
+          600: "rgb(var(--color-primary-600))",
+          700: "rgb(var(--color-primary-700))",
+          800: "rgb(var(--color-primary-800))",
+          900: "rgb(var(--color-primary-900))",
+        },
+        secondary: {
+          0: "rgb(var(--color-secondary-0))",
+          50: "rgb(var(--color-secondary-50))",
+          100: "rgb(var(--color-secondary-100))",
+          200: "rgb(var(--color-secondary-200))",
+          300: "rgb(var(--color-secondary-300))",
+          400: "rgb(var(--color-secondary-400))",
+          500: "rgb(var(--color-secondary-500))",
+          600: "rgb(var(--color-secondary-600))",
+          700: "rgb(var(--color-secondary-700))",
+          800: "rgb(var(--color-secondary-800))",
+          900: "rgb(var(--color-secondary-900))",
+        },
       },
     },
   },
